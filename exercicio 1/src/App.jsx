b122ef8892341a6a057ee7af2364b45f562b1abd
GoldGameboi <kyotobr123@gmail.com>
2025-04-28 19:14:49 -0300

    First things first
    Primeiro exercicio do meu aprendizado com React JS

---
+++ exercicio 1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Projeto com React JS</h1>
+      <p>Hello World!</p>
+    </>
+  )
+}
+
+export default App
